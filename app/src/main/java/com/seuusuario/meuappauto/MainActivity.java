3fb0a8ea2a87e5f0825b7245ac9efffd7e90ad71
jnautosom80-hue <jnautosom80@gmail.com>
2026-06-13 07:09:56 -0300

    Add MainActivity to load HTML from assets

---
+++ app/src/main/java/com/seuusuario/meuappauto/MainActivity.java
@@ -0,0 +1,22 @@
+package com.seuusuario.meuappauto;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true); // Permite rodar scripts do seu HTML
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Carrega o seu arquivo index.html da pasta assets
+        webView.loadUrl("file:///android_asset/index.html");
+        
+        setContentView(webView);
+    }
+}
